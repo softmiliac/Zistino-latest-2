a3dee51766cfd9369522a84ac1667cfae3868f06
softmiliac <110964505+softmiliac@users.noreply.github.com>
2025-12-02 21:54:52 +0330

    Update apps.py for auth app

--- zistino_backend_api/zistino_backend/zistino_apps/authentication/apps.py
+++ zistino_backend_api/zistino_backend/zistino_apps/authentication/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'zistino_apps.authentication'
+    
+    def ready(self):
+        """Import signals when app is ready."""
+        import zistino_apps.authentication.signals
